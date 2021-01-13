74098e2ccf010b98b3e978786e0359818fde02bf
cmf <191237311@qq.com>
2021-01-13 09:57:14 +0800

    1

--- anbao/src/view/play/index.vue
+++ anbao/src/view/play/index.vue
@@ -1,7 +1,21 @@
 <template>
 	<div class="view_play">
 		<img class="bg" :src="bgList[bgActive]" alt="">
-		<div class="main"></div>
+		<div class="main">
+			<div class="tops"></div>
+			<div class="play">
+				<div class="pan">
+					<img src="../../assets/img/others/2.png" alt="">
+					<img src="../../assets/img/others/6.png" alt="">
+					<img src="../../assets/img/others/8.png" alt="">
+				</div>
+				<div class="zw" v-if="false">
+					<div class="top">
+						<img src="../../assets/img/others/8.png" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -30,13 +44,21 @@
 			display: block;
 			width: 100%;
 			height: 100%;
-			.main{
-				position: absolute;
-				top: 0;
-				left: 0;
-				width: 100%;
-				height: 100%;
-			}
 		}	
+		.main{
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			.pan{
+				position: relative;
+				padding: 20px 60px;
+				img:first-child{
+					display: block;
+					width: 100%;
+				}
+			}
+		}
 	}
 </style>
